49dbf173ab52caa91f379641140e5393fb27f8ba
LizaLaugardHald <liza.voros@hotmail.com>
2022-09-25 16:13:02 +0200

    Scikit-learn
     Using Scikit-learn to print the keys, number of rows-columns, feature names and the description of the Iris data

--- MachineLearning_IrisFlower/MachineLearning_IrisFlower/MachineLearning_IrisFlower.py
+++ MachineLearning_IrisFlower/MachineLearning_IrisFlower/MachineLearning_IrisFlower.py
@@ -5,19 +5,12 @@ file = open("C:\\Users\\Andreas\\Documents\\GitHub\\MachineLearning_IrisFlower\\
 
 type(file)
 csvreader = pd.read_csv(file)
-
-#header = []
-#header = next(csvreader)
-#print(header)
-
-#rows = []
-#for row in csvreader:
-#        rows.append(row)
-#for i in range(0,len(rows)):
-#    print(str(rows[i]) + '\n')
     
 print("\nData Type:")
 print(type(file))
 print(csvreader.head(3))
 
 file.close()
+
+print(csvreader.shape)
+print(csvreader.keys)
