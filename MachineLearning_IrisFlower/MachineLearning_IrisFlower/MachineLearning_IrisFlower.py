2c205640785d1409a865373c5c45c53bb5846a3e
LizaLaugardHald <liza.voros@hotmail.com>
2022-09-25 14:39:46 +0200

    New way to load data

--- MachineLearning_IrisFlower/MachineLearning_IrisFlower/MachineLearning_IrisFlower.py
+++ MachineLearning_IrisFlower/MachineLearning_IrisFlower/MachineLearning_IrisFlower.py
@@ -1,22 +1,23 @@
-import csv
+#import csv
+import pandas as pd
 
 file = open("C:\\Users\\Andreas\\Documents\\GitHub\\MachineLearning_IrisFlower\\MachineLearning_IrisFlower\\Data\\iris.csv")
 
 type(file)
-csvreader = csv.reader(file)
+csvreader = pd.read_csv(file)
 
-header = []
-header = next(csvreader)
-print(header)
+#header = []
+#header = next(csvreader)
+#print(header)
 
-rows = []
-for row in csvreader:
-        rows.append(row)
-for i in range(0,len(rows)):
-    print(str(rows[i]) + '\n')
+#rows = []
+#for row in csvreader:
+#        rows.append(row)
+#for i in range(0,len(rows)):
+#    print(str(rows[i]) + '\n')
     
-
-#print(rows + '\n')
+print("\nData Type:")
+print(type(file))
+print(csvreader.head(3))
 
 file.close()
-
